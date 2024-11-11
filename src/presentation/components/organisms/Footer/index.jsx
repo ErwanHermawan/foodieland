94bd691a152cde462ad68b9bc9cd953845c09424
ErwanHermawan <erwanhermawan858@gmail.com>
2024-11-11 14:29:52 +0700

    update footer

--- src/presentation/components/organisms/Footer/index.jsx
+++ src/presentation/components/organisms/Footer/index.jsx
@@ -12,21 +12,35 @@ const Footer = (props) => {
 	return (
 		<footer className={style.footer}>
 			<div className="container">
-				<div className={style.about}>
-					<Link href={footerData.logo.to}>
-						<Image src={footerData.logo.image} alt="Foodieland" />
-					</Link>
-					<p className={style.description}>{footerData.description}</p>
+				<div className={style.top}>
+					<div className={style.about}>
+						<Link className={style.logo} href={footerData.logo.to}>
+							<Image src={footerData.logo.image} alt="Foodieland" />
+						</Link>
+						<p className={style.description}>{footerData.description}</p>
+					</div>
+					<ul className={style.menu}>
+						{footerData.menu.map((val, idx) => (
+							<li className={style.item} key={`idx-${idx}`}>
+								<Link href={val.to} className={style.link}>
+									{val.text}
+								</Link>
+							</li>
+						))}
+					</ul>
 				</div>
-				<ul className={style.menu}>
-					{footerData.menu.map((val, idx) => (
-						<li className={style.item} key={`idx-${idx}`}>
-							<Link href={val.to} className={style.link}>
-								{val.text}
-							</Link>
-						</li>
-					))}
-				</ul>
+				<div className={style.bottom}>
+					<p className={style.copyright}>{footerData.copyright}</p>
+					<ul className={style.sosmed}>
+						{footerData.sosmed.map((valS, idxS) => (
+							<li className={style.sosmedItem} key={`s-${idxS}`}>
+								<Link className={style.sosmedLink} href={valS.to}>
+									<i className={`fi-${valS.icon}`}></i>
+								</Link>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</footer>
 	);
